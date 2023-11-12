429eade5a6af5a4665d235816efb1c0e58cb5af8
WindaPratama01 <windapratama2306@gmail.com>
2023-11-12 10:56:40 +0800

    merubah semua tampilan menjadi rapi

--- app/views/templates/sidebar.php
+++ app/views/templates/sidebar.php
@@ -1,6 +1,6 @@
 <body id="page-top">
     <div id="wrapper">
-        <ul class="navbar-nav bg-primary sidebar sidebar-dark accordion bg-danger" id="accordionSidebar">
+        <ul class="navbar-nav bg-primary sidebar sidebar-dark accordion" id="accordionSidebar">
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div class="sidebar-brand-icon rotate-n-15">
                 </div>

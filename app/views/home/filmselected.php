001e1e44180d5f1d69a90b0e8bc13b72d641fdbb
AchaRhmn <AchaRhmn@gmail.com>
2023-11-14 08:05:22 +0800

    rapiin kodingan, walau engga jalan :v

--- app/views/home/filmselected.php
+++ app/views/home/filmselected.php
@@ -143,6 +143,7 @@
                                         <div class="swiper-wrapper">
                                             <?php foreach ($data['movies_rating'] as $movie) { ?>
                                                 <div class="swiper-slide">
+                                                    <img src="<?= BASEURL ?>assets/img/thumbnails/<?= $movie['thumbnail'] ?>" alt="Thumbnail Movie" class="img-fluid rounded" style="aspect-ratio: 2.9/4;">
                                                     <a href="<?= BASEURL ?>filmselected?id=<?= $movie['id']; ?>">
                                                         <div class="wrapper p-3 rounded-bottom" style="background-color: rgba(255,255,255,0.04);">
                                                             <p class="text-light mb-0"><?= $movie['title']; ?> <span class="text-secondary">(<?= $movie['rating']; ?>)</span></p>
@@ -254,7 +255,7 @@
             },
         });
     </script>
-
+    
 </body>
 
 </html>
